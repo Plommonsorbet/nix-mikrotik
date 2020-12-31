3dda9a8613438e779323160d0b669ece5938d63f
Isak Johansson <isak@irq1.se>
2020-12-31 05:04:15 +0100

    REMOVE dont include rust source code in shell.

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ let
 
   rust_nightly = (nixpkgs.latest.rustChannels.nightly.rust.override {
     extensions = [
-      "rust-src"
+      #"rust-src"
       "rust-analysis"
       "clippy-preview"
       "rustfmt-preview"
@@ -16,8 +16,6 @@ let
   });
 in with nixpkgs;
 stdenv.mkDerivation {
-  name = "nix-mikrotik-dev";
+  name = "nix-mikrotik";
   buildInputs = [ rust_nightly rustfmt openssl pkg-config];
-  shellHook = ''
-  '';
 }
